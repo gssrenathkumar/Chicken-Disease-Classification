852cbb7f5accc39617ac7a714adbddc2167a7dd0
gssrenathkumar <srenathindustries215@gmail.com>
2023-06-23 12:25:39 +0530

    Base Model Generated Sucessfully

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from Chicken_Disease_Classifier import logger
 from Chicken_Disease_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Chicken_Disease_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Preparation of Base Model"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>>>>>>>>>>>Stage: {STAGE_NAME} Started Successfully")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>>>>>>>>>Stage: {STAGE_NAME} Completed <<<<<<<\n\nx============x")
 except Exception as e:
     logger.exception(e)
     raise e
---
+++ src/Chicken_Disease_Classifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,17 @@
+from Chicken_Disease_Classifier.config.configuration import ConfigurationManager
+from Chicken_Disease_Classifier.components.prepare_base_model import PrepareBaseModel
+from Chicken_Disease_Classifier import logger
+
+
+STAGE_NAME = "Prepare base model"
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
